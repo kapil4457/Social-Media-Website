545c2929f5e1967476232c1269eb62e7bd920471
Kapil Soni <kapilsoni54768161@gmail.com>
2023-02-25 21:59:40 +0530

    Added the Update post controller and tested it

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -6,9 +6,29 @@ exports.createPost = async(req,res,next)=>{
 
         const {description , isImage , link   } = req.body;
         const post = await Post.create({description , isImage , link});
-        res.status(201).send({success : true , post})
+       return await  res.status(201).send({success : true , post})
 
     }catch(err){
-        res.status(500).send({success : false , message : err.message})
+        return await res.status(500).send({success : false , message : err.message})
     }
 }
+
+
+exports.updatePost = async(req,res,next)=>{
+    try{
+        const {description , _id } = req.body;
+        const updatedData = {
+            description : description
+        }
+        const updatedPost = await Post.findByIdAndUpdate(_id ,updatedData ,{
+            new : true,
+            runValidators : true,
+            useFindAndModify : true
+        })
+        return await res.status(200).send({success : true , updatedPost})
+
+    }catch(err){
+        return await res.status(500).send({success : false  , message : err.message})
+    }
+}
+
--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createPost } = require('../controllers/PostController.js');
+const { createPost, updatePost } = require('../controllers/PostController.js');
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth.js");
 const router = express.Router()
 module.exports = router;
 
 
 router.route("/create/post").post( isAuthenticatedUser ,createPost);
+router.route("/update/post").put( isAuthenticatedUser ,updatePost);
